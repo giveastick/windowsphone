f4ed936032a4a89e03bda13192cfd5fc1a466246
maximelebastard <lebastardmaxime@gmail.com>
2014-05-28 11:49:26 +0200

    Initial commit

---
+++ GiveAStickWP8/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using GiveAStickWP8.Resources;
+
+namespace GiveAStickWP8
+{
+    /// <summary>
+    /// Permet d'accéder aux ressources de chaîne.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
